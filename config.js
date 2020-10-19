94ebfde0380d633c36859d643d21f55f5daf3a99
egarzac <egarzac@github.com>
2020-10-18 19:48:41 -0500

    final version

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+var API_KEY = "pk.eyJ1IjoiZWdhcnphYyIsImEiOiJja2cwNGltemwwajBpMnlvYm96bWk1dmF4In0._aQUIDYzjQocZzeb8RThvQ";
---
+++ logic.js
@@ -0,0 +1,58 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakes) {
+
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  var allEarthquakes = L.geoJSON(earthquakes, {
+    onEachFeature: onEachFeature
+  });
+//Sending all earthquakes to map
+  createMap(allEarthquakes);
+}
+
+function createMap(allEarthquakes) {
+    // Create the tile layer that will be the background of our map
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 12,
+      id: "light-v10",
+      accessToken: API_KEY
+    });
+
+  // Create a baseMaps object to hold the lightmap layer
+  var baseMaps = {
+    "Light Map": lightmap
+  };
+
+  // Create an overlayMaps object to hold the earthquakes layer
+  var overlayMaps = {
+    "allEarthquakes": allEarthquakes
+  };
+
+  // Create the map object with options
+  var map = L.map("map", {
+  //Colorado-Kansas-Nebraska-Tripoint Coordinates
+    center: [40.003198, -102.051785],
+    zoom: 4,
+    layers: [lightmap, allEarthquakes]
+
+})
+// Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+}
